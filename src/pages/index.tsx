fd92a08249283ea59890fd0d3a6f79b625434733
Kelvin Musselli <kelvinmusselli22@gmail.com>
2022-05-03 15:38:23 -0300

    feat: add ssr and ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next'; // renderizar como SSG
+
+// import { GetServerSideProps } from 'next'; // renderizar como SSR
+
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -26,7 +29,7 @@ export default function Home({ product }: HomeProps) {
           <h1>News about the <span>React</span> World.</h1>
           <p>
             Get access to all publications <br />
-            <span> for $ {product.amount} month</span>
+            <span> for {product.amount} month</span>
           </p>
           <SubscribeButton priceId={product.priceId}/>
         </section>
@@ -36,9 +39,9 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KvPZ8FY2CvKpvGjd5SEJI3f', {
-      expand: ['product']
+    expand: ['product']
   });
 
   const product = {
@@ -52,6 +55,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 horas
   }
 }
