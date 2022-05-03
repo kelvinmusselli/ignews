01ce7efffab2233fb185e78999046aca29e5e7f2
Kelvin Musselli <kelvinmusselli22@gmail.com>
2022-05-03 07:53:58 -0300

    feat: add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
-import styles from '../styles/home.module.scss';
 
+import Head from 'next/head';
 export default function Home() {
   return (
-    <h1 className={styles.title}>
+    <h1>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
       Hello
       <span> World</span>
     </h1>
