55ef23cb51fc1f94ad45b5322da18cfc6360b337
Kelvin Musselli <kelvinmusselli22@gmail.com>
2022-05-03 08:41:34 -0300

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
 
 import Head from 'next/head';
+
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <h1>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      Hello
-      <span> World</span>
+
+      <main className={styles.contentContaier}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> World.</h1>
+          <p>
+            Get access to all publications <br />
+            <span> for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </h1>
   )
 }
