a4a4e506b3fae00795135b17228a56fe4bf27ba5
Kelvin Musselli <kelvinmusselli22@gmail.com>
2022-05-04 10:35:35 -0300

    feat: add faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,54 @@
-import NextAuth from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
+import { query as q } from 'faunadb';
+
+import NextAuth from 'next-auth';
+
+import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
-})
+  callbacks:{
+    async signIn({ user: { email } }) {
+      const isAllowedToSignIn = true
+      if (isAllowedToSignIn) {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('users_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ), // a partir daqui é dentro do if
+            q.Create(
+              q.Collection('users'), {
+                data: { email }
+              }
+            ), // a partir daqui é dentro do else
+            q.Get(
+              q.Match(
+                q.Index('users_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+          // q.Create( // salva no db
+          //   q.Collection('users'), {
+          //     data: { email }
+          //   }
+          // )
+        )
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+});
